18d390983298a03bb56ae38b60011447756680bd
Sanjeev <srgorla@wisc.edu>
2025-11-05 19:21:25 -0600

    fixed base in config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: '/210/',
+  base: '/p182/',
   build: {
     outDir: 'docs'
   }
