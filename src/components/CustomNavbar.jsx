2d1adf3dca4b6b403abeb5ffc338c2388929ced2
Sanjeev <srgorla@wisc.edu>
2025-11-19 16:20:50 -0600

    navbar fix

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -5,11 +5,10 @@ export default function CustomNavbar() {
   return (
     <Navbar bg="primary" variant="dark" expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand as={Link} to="/">UW Habitat for Humanity</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/p182">UW Habitat for Humanity</Navbar.Brand>
         <Navbar.Toggle aria-controls="main-navbar-nav" />
         <Navbar.Collapse id="main-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/p182">Home</Nav.Link>
             <Nav.Link as={Link} to="/p182/who-we-are">Who We Are</Nav.Link>
             <Nav.Link as={Link} to="/p182/get-involved">Get Involved</Nav.Link>
             <Nav.Link as={Link} to="/p182/affiliates">Affiliates</Nav.Link>
