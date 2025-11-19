eeebff2cef4e466379c125adb1c981c1281db928
Sanjeev <srgorla@wisc.edu>
2025-11-19 16:42:55 -0600

    another navbar fix

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -9,9 +9,9 @@ export default function CustomNavbar() {
         <Navbar.Toggle aria-controls="main-navbar-nav" />
         <Navbar.Collapse id="main-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/p182/who-we-are">Who We Are</Nav.Link>
-            <Nav.Link as={Link} to="/p182/get-involved">Get Involved</Nav.Link>
-            <Nav.Link as={Link} to="/p182/affiliates">Affiliates</Nav.Link>
+            <Nav.Link as={Link} to="/who-we-are">Who We Are</Nav.Link>
+            <Nav.Link as={Link} to="/get-involved">Get Involved</Nav.Link>
+            <Nav.Link as={Link} to="/affiliates">Affiliates</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
