24df321195ed1d7a699ae516077a59366ab8bc11
Sanjeev <srgorla@wisc.edu>
2025-11-19 16:32:57 -0600

    hashrouter debugging

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -5,13 +5,13 @@ export default function CustomNavbar() {
   return (
     <Navbar bg="primary" variant="dark" expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand as={Link} to="/">UW Habitat for Humanity</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/p182/">UW Habitat for Humanity</Navbar.Brand>
         <Navbar.Toggle aria-controls="main-navbar-nav" />
         <Navbar.Collapse id="main-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/who-we-are">Who We Are</Nav.Link>
-            <Nav.Link as={Link} to="/get-involved">Get Involved</Nav.Link>
-            <Nav.Link as={Link} to="/affiliates">Affiliates</Nav.Link>
+            <Nav.Link as={Link} to="/p182/who-we-are">Who We Are</Nav.Link>
+            <Nav.Link as={Link} to="/p182/get-involved">Get Involved</Nav.Link>
+            <Nav.Link as={Link} to="/p182/affiliates">Affiliates</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
